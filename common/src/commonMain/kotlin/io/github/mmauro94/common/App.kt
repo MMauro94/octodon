ab68b70a0774dd5955de3afec770102a30eb65f9
Mauro Molin <molin.mauro@gmail.com>
2023-06-17 18:15:25 +0100

    Added app main screen mockup

--- common/src/commonMain/kotlin/io/github/mmauro94/common/App.kt
+++ common/src/commonMain/kotlin/io/github/mmauro94/common/App.kt
@@ -1,21 +1,47 @@
 package io.github.mmauro94.common
 
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Mail
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+
+private enum class Screens(val icon: ImageVector) {
+    HOME(Icons.Default.Home),
+    INBOX(Icons.Default.Mail),
+    SAVED(Icons.Default.Star),
+    PROFILE(Icons.Default.Person),
+}
 
 @Composable
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-    val platformName = getPlatformName()
-
-    Button(onClick = {
-        text = "Hello, $platformName"
-    }) {
-        Text(text)
+    var currentScreen by remember { mutableStateOf(Screens.HOME) }
+    Column {
+        Text(currentScreen.name, Modifier.weight(1f))
+        NavigationBar {
+            Screens.values().forEach { screen ->
+                NavigationBarItem(
+                    selected = screen == currentScreen,
+                    icon = { Icon(screen.icon, null) },
+                    label = { Text(screen.name.lowercase().replaceFirstChar { it.uppercase() }) },
+                    onClick = {
+                        currentScreen = screen
+                    },
+                    alwaysShowLabel = false,
+                )
+            }
+        }
     }
 }

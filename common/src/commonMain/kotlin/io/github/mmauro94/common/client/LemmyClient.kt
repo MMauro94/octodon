392441aba00662462856e3647d4f8fef6f9f78cd
Mauro Molin <molin.mauro@gmail.com>
2023-06-20 21:10:30 +0100

    Add basic GetPosts unit test

--- common/src/commonMain/kotlin/io/github/mmauro94/common/client/LemmyClient.kt
+++ common/src/commonMain/kotlin/io/github/mmauro94/common/client/LemmyClient.kt
@@ -1,6 +1,9 @@
 package io.github.mmauro94.common.client
 
 import io.ktor.client.HttpClient
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.HttpClientEngineConfig
+import io.ktor.client.engine.HttpClientEngineFactory
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.compression.ContentEncoding
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
@@ -8,12 +11,22 @@ import io.ktor.client.plugins.defaultRequest
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
-data class LemmyClient(
+class LemmyClient(
     val url: String,
+    engineFactory: HttpClientEngineFactory<*> = OkHttp,
 ) {
 
+    constructor(url: String, engine: HttpClientEngine) : this(
+        url = url,
+        engineFactory = object : HttpClientEngineFactory<HttpClientEngineConfig> {
+            override fun create(block: HttpClientEngineConfig.() -> Unit): HttpClientEngine {
+                return engine
+            }
+        },
+    )
+
     val ktorClient by lazy {
-        HttpClient(OkHttp) {
+        HttpClient(engineFactory) {
             expectSuccess = true
             defaultRequest {
                 url(this@LemmyClient.url + "api/v3/")
--- common/src/commonTest/kotlin/io/github/mmauro94/common/client/api/GetPostsTest.kt
+++ common/src/commonTest/kotlin/io/github/mmauro94/common/client/api/GetPostsTest.kt
@@ -1,13 +1,29 @@
 package io.github.mmauro94.common.client.api
 
+import io.github.mmauro94.common.client.LemmyClient
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.shouldBe
+import io.ktor.client.engine.mock.MockEngine
+import io.ktor.client.engine.mock.respond
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.headersOf
+import io.ktor.utils.io.ByteReadChannel
 
 class GetPostsTest : FunSpec(
     {
 
-        test("1+1=2") {
-            1 + 1 shouldBe 2
+        test("test posts api doesn't throw") {
+            val postsJson = javaClass.getResource("/api_examples/get_posts.json")!!.readText()
+            val mockEngine = MockEngine { request ->
+                respond(
+                    content = ByteReadChannel(postsJson),
+                    status = HttpStatusCode.OK,
+                    headers = headersOf(HttpHeaders.ContentType, "application/json"),
+                )
+            }
+            val client = LemmyClient("", mockEngine)
+
+            client.getPosts()
         }
     },
 )

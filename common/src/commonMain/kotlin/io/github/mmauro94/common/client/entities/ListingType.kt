b5374b591cb2ec7bf205e3c853923b32ac3262a5
Mauro Molin <molin.mauro@gmail.com>
2023-06-26 21:28:15 +0100

    Add lowercase variant supported in all enums (#17)

--- common/src/commonMain/kotlin/io/github/mmauro94/common/client/entities/ListingType.kt
+++ common/src/commonMain/kotlin/io/github/mmauro94/common/client/entities/ListingType.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalSerializationApi::class)
+
 package io.github.mmauro94.common.client.entities
 
 import androidx.compose.material.icons.Icons
@@ -8,17 +10,22 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import io.github.mmauro94.common.MR
 import io.github.mmauro94.common.utils.ComposableString
 import io.github.mmauro94.common.utils.composable
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonNames
 
 @Serializable
 enum class ListingType(val label: ComposableString, val icon: ImageVector) {
     @SerialName("Subscribed")
+    @JsonNames("subscribed")
     SUBSCRIBED(MR.strings.listing_type_subscribed.composable(), Icons.Filled.Home),
 
     @SerialName("Local")
+    @JsonNames("local")
     LOCAL(MR.strings.listing_type_local.composable(), Icons.Default.LocationCity),
 
     @SerialName("All")
+    @JsonNames("all")
     ALL(MR.strings.listing_type_all.composable(), Icons.Default.Public),
 }
--- common/src/commonMain/kotlin/io/github/mmauro94/common/client/entities/RegistrationMode.kt
+++ common/src/commonMain/kotlin/io/github/mmauro94/common/client/entities/RegistrationMode.kt
@@ -1,17 +1,24 @@
+@file:OptIn(ExperimentalSerializationApi::class)
+
 package io.github.mmauro94.common.client.entities
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonNames
 
 @Serializable
 enum class RegistrationMode {
 
     @SerialName("Closed")
+    @JsonNames("closed")
     CLOSED,
 
     @SerialName("RequireApplication")
+    @JsonNames("requireapplication")
     REQUIRE_APPLICATION,
 
     @SerialName("Open")
+    @JsonNames("open")
     OPEN,
 }
